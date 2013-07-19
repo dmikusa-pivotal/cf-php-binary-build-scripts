fcd591819b0971dd3911b1fe560d9b56c8defdb3
Daniel Mikusa <dmikusa@gopivotal.com>
2013-07-19 11:17:33 -0700

    Added generation of shasum file

--- httpd-2.2/build.sh
+++ httpd-2.2/build.sh
@@ -31,8 +31,7 @@ cd ../
 # Build mod_fcgid
 cd mod_fcgid-2.3.7
 make clean
-APXS=/tmp/staged/app/httpd/bin/apxs 
-./configure.apxs 
+APXS=/tmp/staged/app/httpd/bin/apxs ./configure.apxs 
 make
 make install
 cd ../
@@ -44,18 +43,33 @@ rm -rf conf/extra/* conf/httpd.conf conf/httpd.conf.bak conf/magic conf/original
 
 # Fix start scripts
 cd /tmp/staged/app/httpd/bin
+
 # fix hard-coded path with env variable
-sed 's/\/tmp\/staged\/app/\${HOME}/g' apachectl > apachectl
-sed 's/\/tmp\/staged\/app/\${HOME}/g' apr-1-config > apr-1-config
-sed 's/\/tmp\/staged\/app/\${HOME}/g' apu-1-config > apu-1-config
-sed 's/\/tmp\/staged\/app/\${HOME}/g' envvars > envvars
-sed 's/\/tmp\/staged\/app/\${HOME}/g' envvars-std > envvars-std
+sed 's/\/tmp\/staged\/app/\${HOME}/g' apachectl > apachectl.fixed
+mv apachectl.fixed apachectl
+chmod 755 apachectl
+sed 's/\/tmp\/staged\/app/\${HOME}/g' apr-1-config > apr-1-config.fixed
+mv apr-1-config.fixed apr-1-config
+chmod 755 apr-1-config
+sed 's/\/tmp\/staged\/app/\${HOME}/g' apu-1-config > apu-1-config.fixed
+mv apu-1-config.fixed apu-1-config
+chmod 755 apu-1-config
+sed 's/\/tmp\/staged\/app/\${HOME}/g' envvars > envvars.fixed
+mv envvars.fixed envvars
+chmod 755 envvars
+sed 's/\/tmp\/staged\/app/\${HOME}/g' envvars-std > envvars-std.fixed
+mv envvars-std.fixed envvars-std
+chmod 755 envvars-std
+
 # switch single to double quotes so variable expansion occurs
-sed -r "s/HTTPD='(.*)'/HTTPD=\"\1\"/g" apachectl > apachectl
+sed -r "s/HTTPD='(.*)'/HTTPD=\"\1\"/g" apachectl > apachectl.fixed
+mv apachectl.fixed apachectl
+chmod 755 apachectl
 
 # Package the binary
 cd /tmp/staged/app
 mv httpd httpd-$VERSION-bin
 tar czf httpd-$VERSION-bin.tar.gz httpd-$VERSION-bin
+shasum httpd-$VERSION-bin.tar.gz > httpd-$VERSION-bin.tar.gz.sha1
 
 echo "Done!"
