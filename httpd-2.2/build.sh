6ab55e50bc7d010efe81aefd25b665c32b0fb65e
Daniel Mikusa <dmikusa@gopivotal.com>
2013-07-19 08:01:50 -0700

    Initial repo, contains build instructions for HTTPD & PHP

---
+++ httpd-2.2/build.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+VERSION="$1"
+if [ "n$VERSION" == "n" ]; then
+    echo "No version specified"
+    exit -1
+fi
+
+# curl download file - follow redirect, output to file
+if [ -f httpd-$VERSION.tar.bz2 ]; then
+    rm httpd-$VERSION.tar.bz2
+fi
+if [ ! -d httpd-$VERSION ]; then
+    curl -L -o httpd-$VERSION.tar.bz2 http://apache.osuosl.org//httpd/httpd-$VERSION.tar.bz2
+fi
+
+tar jxf httpd-$VERSION.tar.bz2
+rm httpd-$VERSION.tar.bz2
+cd httpd-$VERSION
+
+# Build HTTPD 2.2.x
+./configure \
+	--prefix=/tmp/staged/app/httpd \
+	--enable-mods-shared=all \
+	--enable-so \
+	--with-mpm=worker
+make
+make install
+cd ../
+
+# Build mod_fcgid
+cd mod_fcgid-2.3.7
+make clean
+APXS=/tmp/staged/app/httpd/bin/apxs 
+./configure.apxs 
+make
+make install
+cd ../
+
+# Remove unnecessary files and config
+cd /tmp/staged/app/httpd
+rm -rf build cgi-bin/ error/ icons/ include/ man/ manual/ htdocs/
+rm -rf conf/extra/* conf/httpd.conf conf/httpd.conf.bak conf/magic conf/original
+
+# Fix start scripts
+cd /tmp/staged/app/httpd/bin
+# fix hard-coded path with env variable
+sed 's/\/tmp\/staged\/app/\${HOME}/g' apachectl > apachectl
+sed 's/\/tmp\/staged\/app/\${HOME}/g' apr-1-config > apr-1-config
+sed 's/\/tmp\/staged\/app/\${HOME}/g' apu-1-config > apu-1-config
+sed 's/\/tmp\/staged\/app/\${HOME}/g' envvars > envvars
+sed 's/\/tmp\/staged\/app/\${HOME}/g' envvars-std > envvars-std
+# switch single to double quotes so variable expansion occurs
+sed -r "s/HTTPD='(.*)'/HTTPD=\"\1\"/g" apachectl > apachectl
+
+# Package the binary
+cd /tmp/staged/app
+mv httpd httpd-$VERSION-bin
+tar czf httpd-$VERSION-bin.tar.gz httpd-$VERSION-bin
+
+echo "Done!"
---
+++ php-5.3/build.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+VERSION="$1"
+if [ "n$VERSION" == "n" ]; then
+    echo "No version specified"
+    exit -1
+fi
+
+# follow redirect, output to file
+if [ -f php-$VERSION.tar.bz2 ]; then
+    rm php-$VERSION.tar.bz2
+fi
+if [ ! -d php-$VERSION ]; then
+    curl -L -o php-$VERSION.tar.bz2 http://us2.php.net/get/php-$VERSION.tar.bz2/from/us3.php.net/mirror
+fi
+
+tar jxf php-$VERSION.tar.bz2
+rm php-$VERSION.tar.bz2
+cd php-$VERSION
+
+# Build PHP 5.3
+./configure \
+	--prefix=/tmp/staged/app/php \
+	--with-config-file-path=/home/vcap/app/php/etc \
+	--enable-cli \
+	--enable-ftp \
+	--enable-sockets \
+	--enable-soap \
+	--enable-fileinfo \
+	--enable-bcmath \
+	--enable-calendar \
+	--with-kerberos \
+	--enable-zip \
+	--with-pear \
+	--with-bz2=shared \
+	--with-curl=shared \
+	--enable-dba=shared \
+	--enable-inifile \
+	--enable-flatfile \
+	--with-cdb \
+	--with-gdbm \
+	--with-mcrypt=shared \
+	--with-mhash=shared \
+	--with-mysql=mysqlnd \
+	--with-mysqli=mysqlnd \
+	--with-pdo-mysql=mysqlnd \
+	--with-gd=shared \
+	--with-pdo-pgsql=shared \
+	--with-pgsql=shared \
+	--with-pspell=shared \
+	--with-gettext=shared \
+	--with-gmp=shared \
+	--with-imap=shared \
+	--with-imap-ssl=shared \
+	--with-ldap=shared \
+	--with-ldap-sasl \
+	--enable-mbstring \
+	--enable-mbregex \
+	--enable-exif=shared \
+	--with-openssl=shared \
+	--enable-fpm
+make
+make install
+cd ../
+
+# Copy required libs to /tmp/staged/app
+cp required-libs/* /tmp/staged/app/php/lib/
+
+# Build extensions
+cd php-extensions
+
+# Build RabbitMQ Libs
+cd rabbitmq-c-rabbitmq-c-v0.3.0
+make clean
+./configure --prefix=/tmp/staged/app/librmq
+make
+make install
+cd ../
+
+# build AMQP extension
+cd amqp-1.2.0
+make clean
+/tmp/staged/app/php/bin/phpize
+./configure --with-php-config=/tmp/staged/app/php/bin/php-config --with-librabbitmq-dir=/tmp/staged/app/librmq
+make
+make install
+cd ../
+
+# build APC
+cd APC-3.1.9
+make clean
+/tmp/staged/app/php/bin/phpize
+./configure --with-php-config=/tmp/staged/app/php/bin/php-config
+make
+make install
+cd ../
+
+# build mongo
+cd mongo-1.4.1
+make clean
+/tmp/staged/app/php/bin/phpize
+./configure --with-php-config=/tmp/staged/app/php/bin/php-config
+make
+make install
+cd ../
+
+# build redis
+cd redis-2.2.3
+make clean
+/tmp/staged/app/php/bin/phpize
+./configure --with-php-config=/tmp/staged/app/php/bin/php-config
+make
+make install
+cd ../
+
+# build xdebug
+cd xdebug-2.2.3
+make clean
+/tmp/staged/app/php/bin/phpize
+./configure --with-php-config=/tmp/staged/app/php/bin/php-config
+make
+make install
+cd ../
+
+# Remove unused files
+cd /tmp/staged/app/php
+rm -rf php/ include/
+
+# Build binary
+cd ../
+mv php php-$VERSION-bin
+tar czf php-$VERSION-bin.tar.gz php-$VERSION-bin
+
+echo "Done!"
+
